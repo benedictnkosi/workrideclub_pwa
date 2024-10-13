8d47d4fe548a3257354a7ca3bf3aec3d1d53296a
BenedictNkosi01 <Benedict.nkosi@merlinentertainments.biz>
2024-10-13 18:48:35 +0200

    center

--- app/splash/page.tsx
+++ app/splash/page.tsx
@@ -19,27 +19,42 @@ const Splash: React.FC = () => {
 
   return (
     <div className="container">
-      <div className="splash">
-        <img src="/images/logo.png" alt="logo" className="mt-5 login-image" />
-        <p>Work Ride Club is a platform that connects drivers and passengers</p>
-        <Button
-          color="light"
-          pill
-          onClick={() => navigateToLogin()}
-          className="login-button"
-        >
-          <FaWhatsapp className="mr-2 small-icon" color="green" />
-          Login with Whatsapp
-        </Button>
-        <Button
-          color="light"
-          pill
-          onClick={() => navigateToRegister()}
-          className="login-button"
-        >
-          <FaWhatsapp className="mr-2 small-icon" color="green" />
-          Register with Whatsapp
-        </Button>
+      <div className="logo-container">
+        <img src="/images/carpool.png" alt="logo" className="mt-5 logo-image" />
+      </div>
+      <div
+        className="content-container"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          height: "100vh",
+        }}
+      >
+        <div className="splash">
+          <img src="/images/logo.png" alt="logo" className="mt-5 login-image" />
+          <p>
+            Work Ride Club is a platform that connects drivers and passengers
+          </p>
+          <Button
+            color="light"
+            pill
+            onClick={() => navigateToLogin()}
+            className="login-button"
+          >
+            <FaWhatsapp className="mr-2 small-icon" color="green" />
+            Login with Whatsapp
+          </Button>
+          <Button
+            color="light"
+            pill
+            onClick={() => navigateToRegister()}
+            className="login-button"
+          >
+            <FaWhatsapp className="mr-2 small-icon" color="green" />
+            Register with Whatsapp
+          </Button>
+        </div>
       </div>
     </div>
   );
